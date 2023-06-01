4b04d4cd5084386f5761bd7db71e6ab24d71e817
sidharth1213 <siddharth.soni@cobold.in>
2023-06-02 02:09:06 +0530

    SS: fist side of for is ready

--- src/components/CarouselContainer.tsx
+++ src/components/CarouselContainer.tsx
@@ -1,10 +1,82 @@
 import React from "react";
-import img from "../images/background-img.png";
+import { Carousel, Typography } from "antd";
+import bgImage from "../images/background-img.png";
+import logo from "../images/logo.png";
+import style from "./style.module.scss";
+import facebookIcon from "../images/icons/facebook.svg";
+import linkedinIcon from "../images/icons/linkedin.svg";
+import instaIcon from "../images/icons/instagram.svg";
+import { AiOutlineGlobal } from "react-icons/ai";
+
+const { Title } = Typography;
+
 const CarouselContainer = () => {
   return (
-    <div className="w-1/2 h-full">
-      <div className="imgContainer ">
-        <img src={img} alt="" className="w-full h-full cover" />
+    <div className="w-1/2 h-full relative p-5 text-white rounded-xl overflow-hidden flex flex-col justify-between">
+      <img
+        src={bgImage}
+        alt="background-image"
+        className="w-full h-full object-cover absolute top-0 left-0 -z-10"
+      />
+
+      <div className="w-[10%]">
+        <img src={logo} alt="logo" className="w-full object-contain" />
+      </div>
+      <div>
+        <Carousel
+          autoplay
+          className={`${style.typography} text-white text-2xl h-4/6`}
+        >
+          <div>
+            <Title level={1}>100% Uptime😎</Title>
+            <Title level={2} type="secondary">
+              Zero Infrastructure
+            </Title>
+            <Title level={2} type="secondary">
+              Management
+            </Title>
+          </div>
+          <div>
+            <Title level={1}>100% Uptime😎</Title>
+            <Title level={2} type="secondary">
+              Zero Infrastructure
+            </Title>
+            <Title level={2} type="secondary">
+              Management
+            </Title>
+          </div>
+          <div>
+            <Title level={1}>100% Uptime😎</Title>
+            <Title level={2} type="secondary">
+              Zero Infrastructure
+            </Title>
+            <Title level={2} type="secondary">
+              Management
+            </Title>
+          </div>
+        </Carousel>
+        <div className="mt-24 flex justify-between items-center">
+          <div>
+            <a
+              href="https://aesthisia.com/"
+              className="flex space-x-1 items-center"
+            >
+              <AiOutlineGlobal size={20} />
+              <p>aesthisia.com</p>
+            </a>
+          </div>
+          <div className=" flex space-x-3 ">
+            <a href="https://www.linkedin.com/company/aesthisia/">
+              <img src={linkedinIcon} alt="linkedinIcon" className="w-4" />
+            </a>
+            <a href="https://www.facebook.com/aesthisia/">
+              <img src={facebookIcon} alt="facebookIcon" className="w-4" />
+            </a>
+            <a href="https://www.instagram.com/aesthisia/">
+              <img src={instaIcon} alt="facebookIcon" className="w-4" />
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
